7d3cb3c995b56887d4f93612f32c880191842f45
NotSoSiraj <115099886+NotSoSiraj@users.noreply.github.com>
2023-06-18 18:43:03 +0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -12,5 +12,3 @@ signupForm.addEventListener('submit', (e) => {
 
  */
 
-// Assuming you have a container element with the id "articles-container" in your HTML
-// Assuming you have a container element with the ID "articles-container" in your HTML
